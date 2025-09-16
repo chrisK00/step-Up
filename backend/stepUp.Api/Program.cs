d3b4d6130fd8f23d23828426751951a38ee5dc0e
ChrisK00 <christian.khalil2000@gmail.com>
2025-09-16 22:24:41 +0200

    log

--- backend/stepUp.Api/Program.cs
+++ backend/stepUp.Api/Program.cs
@@ -21,6 +21,7 @@ builder.Services.Configure<JsonOptions>(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter())
 );
 
+builder.Logging.AddConsole();
 builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddAppServices(builder.Configuration);
